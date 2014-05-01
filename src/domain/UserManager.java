82a2a4459c1be2f2add13a27ebb22736513d99cd
emilfrisk <emilfrisk@10.126.33.123>
2014-05-01 12:42:20 +0200

    updated userManager & User

--- src/domain/UserManager.java
+++ src/domain/UserManager.java
@@ -6,22 +6,26 @@ import java.util.List;
 public class UserManager {
 
         public void addPointsToUsers(List<Transaction> transactions){
-            
+                for (Transaction transaction : transactions) {
+                    int userId = transaction.getUserId();
+                    User user = new User();
+                    user.getUser(userId);
+                    double amount = transaction.getAmount();
+                    double points = amount*
+                }
 //                for (hvert transaktion i listen)
 //                  hent bruger
 //                  initialiser nyt bruger objekt
 //                  tilføj point
 //                  skriv bruger.
-            
-            // HEJ JAKOB
-            // HEJ EMIL
         }
     
 	public void CheckUserLevel() {
 		throw new UnsupportedOperationException();
 	}
 
-	public User createUser(int level, Date creationDate, Point pointBalance, double amountSpenThisYear, String name, int userId, String macaddress, String email, String tlf, String address) {
+	public User createUser(int level, Date creationDate, Point pointBalance, double amountSpenThisYear, 
+                String name, int userId, String macaddress, String email, String tlf, String address) {
 		throw new UnsupportedOperationException();
 	}
 
