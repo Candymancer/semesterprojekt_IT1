cd94da4ce45cb946d0c185057a6c004436c48fc1
emilfrisk <emilfrisk@192.168.1.138>
2014-05-06 12:48:46 +0200

    rettet klasser + begyndt på dbFACADE

--- src/storage/DatabaseFacade.java
+++ src/storage/DatabaseFacade.java
@@ -1,28 +1,98 @@
 package storage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
-
-
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 public class DatabaseFacade {
 
-	public void createTransaction() {
-		throw new UnsupportedOperationException();
-	}
+    private final String db = "emilfrisk";
+    private final String user = "emilfrisk";
+    private final String password = "";
 
-	public ResultSet findUser(int userId) {
-		throw new UnsupportedOperationException();
-	}
+    public void createTransaction() {
+        throw new UnsupportedOperationException();
+    }
 
-	public void writeUser() {
-		throw new UnsupportedOperationException();
-	}
+    public ResultSet findUser(int userId) {
+        throw new UnsupportedOperationException();
+    }
 
-	public void writeTransaction() {
-		throw new UnsupportedOperationException();
-	}
+    public void writeUser() {
+        throw new UnsupportedOperationException();
+    }
 
-	public ResultSet getExpiredTransactions() {
-		throw new UnsupportedOperationException();
-	}
+    public void writeTransaction() {
+        throw new UnsupportedOperationException();
+    }
+    
+    private ResultSet executeQuery(String query) {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:postgresql://localhost/" + db;
+
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("" + query);
+
+        } catch (SQLException e) {
+            if (e != null) {
+                JOptionPane.showMessageDialog(null, "Fejl"); // lav popup
+            }
+        } finally {
+            try {
+////                if (rs != null) {
+////                    rs.close();
+////                }
+//                if (st != null) {
+//                    st.close();
+//                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(DatabaseFacade.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+        return rs;
+    }
+    
+    public ResultSet getExpiredTransactions() {
+
+        Calendar calendar = Calendar.getInstance();
+
+        int day = (24 * 60 * 60 + 1) * 1000;
+
+        long time = calendar.getTimeInMillis() - day;
+
+        Timestamp timestamp = new Timestamp(time);
+
+        String query = "SELECT \n"
+                + "  transactions.date\n"
+                + "FROM \n"
+                + "  public.transaction\n"
+                + "WHERE \n"
+                + "  transaction.date >= '" + timestamp + "'\n"
+                + "	AND transaction.date <= '" + timestamp + day + "';";
+
+        ResultSet rs = executeQuery(query);
+
+        if (rs == null) {
+            return null;
+        }
+
+        return rs;
+    }
 }
