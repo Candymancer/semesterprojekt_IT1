b1b2e318943326db37f1183da233390aeca723eb
emilfrisk <emilfrisk@192.168.1.138>
2014-05-07 14:31:47 +0200

    added lidt til main metode i gui - igang med at teste

--- src/gui/SemesterProjekt_IT1.java
+++ src/gui/SemesterProjekt_IT1.java
@@ -6,6 +6,15 @@
 
 package gui;
 
+import Parser.TransactionParser;
+import domain.System_;
+import domain.Transaction;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author emilfrisk
@@ -15,8 +24,15 @@ public class SemesterProjekt_IT1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        TransactionParser transactionParser = new TransactionParser();
+        System_ sys = new System_();
+        
+        File file = new File("transactions.xml");
+        
+        List<Transaction> transactionList = transactionParser.readFile(file);
+        
+        sys.receiveTransactions(transactionList);
     }
     
 }
