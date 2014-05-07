38dfe4992b214e5fe09d8d2baf9d43a6c72ad851
emilfrisk <emilfrisk@192.168.1.138>
2014-05-07 09:42:34 +0200

    UserManager - ALL DONE

--- src/domain/UserManager.java
+++ src/domain/UserManager.java
@@ -3,15 +3,16 @@ package domain;
 import domain.userLevel.Bronze;
 import domain.userLevel.Gold;
 import domain.userLevel.Silver;
-import java.util.Date;
 import java.util.List;
 
 public class UserManager {
+    
+    User user = null;
 
     public void addPointsToUsers(List<Transaction> transactions) {
         for (Transaction transaction : transactions) {
             int userId = transaction.getUserId();
-            User user = new User();
+            user = new User();
             user.getUser(userId);
             double amount = transaction.getAmount();
             double conversionRate = user.getLevel().getConversionRate();
@@ -34,7 +35,7 @@ public class UserManager {
     public void subtractPointsFromUsers(List<Transaction> transactions) {
         for (Transaction transaction : transactions) {
             int userId = transaction.getUserId();
-            User user = new User();
+            user = new User();
             user.getUser(userId);
             double amount = transaction.getAmount();
             double userAmount = user.getAmountSpentThisYear();
@@ -48,18 +49,5 @@ public class UserManager {
             }
             user.write();
         }
-    }
-
-    public void CheckUserLevel() {
-        throw new UnsupportedOperationException();
-    }
-
-    public User createUser(int level, Date creationDate, Point pointBalance, double amountSpenThisYear,
-            String name, int userId, String macaddress, String email, String tlf, String address) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void updateUserLevel(int level) {
-        throw new UnsupportedOperationException();
     }
 }
